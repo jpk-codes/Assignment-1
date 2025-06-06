1dad4f823b5d2a66f2c0e8b2cf23a98a52f41ba7
jpk-codes <jayaprakash.b.dev@gmail.com>
2025-06-06 23:37:02 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,7 @@
+# Task 2
+first = input("Enter your first name: ")
+second = input("Enter your second name: ")
+
+full_name = first + second
+
+print("Hello, "+full_name+"! Welcome to the Python program.")
